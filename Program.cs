f4183617c5ca2aa52db9cb44b6438e39094ad16d
James Boyden <j.boyden@craneware.com>
2017-12-01 11:38:34 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace AdventOfCode
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = File.ReadAllText("input.txt");
+            var captchaList = input
+                .ToCharArray()
+                .Select(x => int.Parse(x.ToString())).ToArray();
+            var count = 0;
+            var wrap = captchaList.Length / 2;
+            for (int i = 0; i < captchaList.Length; i++)
+            {
+                var curr = captchaList[i];
+                var stepLeft = captchaList.Length - 1 - i;
+                var thisWrap = stepLeft - wrap;
+                var comp = thisWrap < 0 ? captchaList[thisWrap * -1 - 1] : captchaList[captchaList.Length - 1 - thisWrap];
+                count += curr == comp ? curr : 0;
+            }
+
+            Console.WriteLine(count);
+            Console.Read();
+        }
+    }
+}
