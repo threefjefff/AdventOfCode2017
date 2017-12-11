a2fdbd883d0ed3a2935d15f0c45cd09bbcb91bfc
Unknown <jsmboyden@gmail.com>
2017-12-11 11:43:35 +0000

    Day 10 & Day 11

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -5,43 +5,72 @@ using System.Runtime.InteropServices.ComTypes;
 
 namespace Day_10
 {
+    using System.Text;
+
     class Program
     {
-        private static IEnumerable<int> Input => "3,4,1,5"//"192,69,168,160,78,1,166,28,0,83,198,2,254,255,41,12"
+        private static IEnumerable<int> Input => "192,69,168,160,78,1,166,28,0,83,198,2,254,255,41,12"
+            .Select(x => (int)x)
+            .Concat(KeySequence);
+
+        private static IEnumerable<int> KeySequence => "17,31,73,47,23"
             .Split(',')
             .Select(int.Parse);
 
-        private const int MaxIndex = 5;
+        private const int MaxIndex = 256;
         private static List<int> _register = Enumerable.Range(0, MaxIndex).ToList();
+        private static int _currentPosition;
+        private static int _skip;
 
         static void Main(string[] args)
         {
-            Part1();
-            Console.WriteLine();
+            Console.WriteLine(FullHash());
+            Console.Read();
         }
 
-        static void Part1()
+        static string FullHash()
         {
-            var startIndex = 0;
-            var skip = 0;
+            for (var i = 0; i < 64; i++)
+            {
+                KnotHashPass();
+            }
+
+            var sb = new StringBuilder();
+            for (var x = 0; x < 16; x++)
+            {
+                var denseDigit = _register[x*16];
+                for (var y = 1; y < 16; y++)
+                {
+                    denseDigit = denseDigit ^ _register[x * 16 + y];
+                }
+                sb.AppendFormat("{0:x2}", denseDigit);
+            }
+            return sb.ToString();
+        }
+
+        static void KnotHashPass()
+        {
             foreach (var length in Input)
             {
-                OverwriteRegisters(startIndex, length);
-                startIndex = WrapIncrement(startIndex, length + skip);
-                skip++;
+                OverwriteRegisters(_currentPosition, length);
+                _currentPosition = WrapIncrement(_currentPosition, length + _skip);
+                _skip++;
             }
         }
 
         static int WrapIncrement(int index, int steps)
         {
-            return index + steps > MaxIndex - 1
-                ? index + steps - MaxIndex - 1
-                : index + steps;
+            var newIndex = index + steps;
+            while (newIndex > MaxIndex)
+            {
+                newIndex -= MaxIndex;
+            }
+            return newIndex;
         }
 
         static void OverwriteRegisters(int index, int steps)
         {
-            var stepsToTake = index + steps > MaxIndex ? MaxIndex- index : steps;
+            var stepsToTake = index + steps > MaxIndex ? MaxIndex - index : steps;
             var stepsLeft = index + steps > MaxIndex ? index + steps - MaxIndex : 0;
             var valuesToOverwrite = _register.GetRange(index, stepsToTake);
             if (stepsLeft > 0)
@@ -50,18 +79,18 @@ namespace Day_10
             }
 
             valuesToOverwrite.Reverse();
-            _register.Clear();
 
             if (stepsLeft > 0)
             {
                 var untouched = index + steps > MaxIndex
                     ? _register.GetRange(stepsLeft, MaxIndex - stepsLeft - stepsToTake)
                     : _register.GetRange(steps, MaxIndex - stepsLeft - stepsToTake);
+                _register.Clear();
                 _register = valuesToOverwrite
                     .Skip(stepsToTake)
                     .Take(stepsLeft)
                     .ToList();
-                valuesToOverwrite.RemoveRange(stepsToTake,stepsLeft);
+                valuesToOverwrite.RemoveRange(stepsToTake, stepsLeft);
                 _register.AddRange(untouched);
                 _register.AddRange(valuesToOverwrite.Take(stepsToTake));
             }
---
+++ Day11/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace Day11
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Runtime.CompilerServices;
+    using System.Security.Policy;
+
+    class Program
+    {
+        private static IEnumerable<string> Input => File.ReadAllText("input.txt")
+            .Split(',');
+
+        struct CubeCoord
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            public int Z { get; set; }
+        }
+
+        private static int _maxDistance;
+
+        static void Main(string[] args)
+        {
+            var childAt = FollowPath();
+            Console.WriteLine(ManhatanCube(childAt));
+            Console.WriteLine(_maxDistance);
+            Console.Read();
+        }
+
+        static CubeCoord FollowPath()
+        {
+            var pos = new CubeCoord();
+            foreach (var direction in Input)
+            {
+                if (direction.Equals("n"))
+                {
+                    pos.Z--;
+                    pos.Y++;
+                }
+                if (direction.Equals("s"))
+                {
+                    pos.Z++;
+                    pos.Y--;
+                }
+                if (direction.Contains("ne"))
+                {
+                    pos.X++;
+                    pos.Z--;
+                }
+                if (direction.Contains("nw"))
+                {
+                    pos.Y++;
+                    pos.X--;
+                }
+                if (direction.Contains("se"))
+                {
+                    pos.Y--;
+                    pos.X++;
+                }
+                if (direction.Contains("sw"))
+                {
+                    pos.X--;
+                    pos.Z++;
+                }
+                var distance = ManhatanCube(pos);
+                _maxDistance = distance > _maxDistance ? distance : _maxDistance;
+            }
+
+            return pos;
+        }
+
+        static int ManhatanCube(CubeCoord pos)
+        {
+            var max = Math.Max(Math.Abs(pos.X), Math.Abs(pos.Y));
+            return Math.Max(max, Math.Abs(pos.Z));
+        }
+    }
+}
